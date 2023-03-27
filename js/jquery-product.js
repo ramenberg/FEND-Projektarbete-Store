78202256f780f5d3d94b1ff0e2d8f43c3ae2d420
Emelie Ramenberg <ramenberg@gmail.com>
2023-03-27 16:34:58 +0200

    Nya jquery för product och  uppdaterad order.html

--- js/jquery-product.js
+++ js/jquery-product.js
@@ -10,6 +10,21 @@ $.getJSON('https://fakestoreapi.com/products/' + productId).done(function (
         price: product.textContent,
         category: product.textContent,
     };
+    // order-right
+    let productItem = `
+    <li class="list-group-item d-flex justify-content-between lh-condensed">
+    <div>
+        <h6 class="my-0">${product.title}</h6>
+    </div>
+    <span class="text-muted invisible">${product.price}</span>
+    </li>
+    <li class="list-group-item d-flex justify-content-between">
+        <span>Totalt (SEK)</span>
+        <strong>${product.price}</strong>
+    </li>
+        `;
+    $('#order-right-info').html(productItem);
+
     sessionStorage.setItem(
         'chosenProduct', // nyckel
         JSON.stringify(chosenProduct)
